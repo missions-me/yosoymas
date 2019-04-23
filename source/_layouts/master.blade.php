8470b72639ddde723a3f63340fa4f95e3798561a
Neil Keena <nkeena1@gmail.com>
2019-04-23 11:40:57 -0700

    add initial landing page

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -4,9 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Yo Soy Mas</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
-    <body>
+    <body class="font-sans leading-normal antialiased m-0">
         @yield('body')
     </body>
 </html>
